297a4a6f704c58e0c520d6a8b96fa73cbce76255
Jonathan Olsen <jonathanjolsen@gmail.com>
2024-01-10 20:17:37 -0700

    Updated Vite Configs for Dev View

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: '/',
+
+  server: {
+    port: 3000,
+  },
+
+  build: {
+    outDir: 'dist',
+  },
+
   plugins: [react()],
 })
